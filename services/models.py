bf42732dfde93c06b7d5395f4145f83c3108de74
David Ochiel <nguonodave@gmail.com>
2024-09-10 20:43:55 +0300

    feat: add service model and make migrations

--- services/models.py
+++ services/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    price_hr = models.DecimalField(decimal_places=2, max_digits=100)
+    date = models.DateTimeField(auto_now=True, null=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
