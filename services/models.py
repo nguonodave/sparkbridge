1ed27e272cc79fca454aa979b4d33fc5610e3e07
David Ochiel <nguonodave@gmail.com>
2024-09-11 16:01:38 +0300

    feat: add field choices in service model

--- services/models.py
+++ services/models.py
@@ -5,6 +5,20 @@ class Service(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
     price_hr = models.DecimalField(decimal_places=2, max_digits=100)
+    choices = (
+        ('Air Conditioner', 'Air Conditioner'),
+        ('Carpentry', 'Carpentry'),
+        ('Electricity', 'Electricity'),
+        ('Gardening', 'Gardening'),
+        ('Home Machines', 'Home Machines'),
+        ('House Keeping', 'House Keeping'),
+        ('Interior Design', 'Interior Design'),
+        ('Locks', 'Locks'),
+        ('Painting', 'Painting'),
+        ('Plumbing', 'Plumbing'),
+        ('Water Heaters', 'Water Heaters'),
+    )
+    field = models.CharField(max_length=30, blank=False, null=False, default="", choices=choices)
     date = models.DateTimeField(auto_now=True, null=False)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
