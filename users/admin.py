0024ba8b093ba2577d93343e57255ba247af606e
David Ochiel <nguonodave@gmail.com>
2024-09-16 19:26:18 +0300

    feat: customize user admin registration

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from . models import User, Customer, Company
+from django.contrib.auth.admin import UserAdmin
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "email")
-
+class UserAdmin(UserAdmin):
+    list_display = ("id", "username", "email", 'is_company', 'is_customer')
+    list_filter = ('is_company', 'is_customer', 'is_staff', 'is_superuser', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('User type', {'fields': ('is_company', 'is_customer')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'is_company', 'is_customer'),
+        }),
+    )
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
