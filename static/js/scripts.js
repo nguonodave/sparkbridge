5f74cf0d6abf874e4d8ac4a1b18d11e4a22c7de0
David Ochiel <nguonodave@gmail.com>
2024-09-20 18:44:57 +0300

    feat(JS): add timer to make messages disappear after 5 seconds

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,11 @@
 function showDeletePopup() {
-    document.getElementById('deletePopup').style.display = 'flex';
+    document.getElementById('deletePopup').style.display = 'flex'
 }
 
 function hideDeletePopup() {
-    document.getElementById('deletePopup').style.display = 'none';
+    document.getElementById('deletePopup').style.display = 'none'
 }
+
+setTimeout(function(){ 
+    document.getElementById("message").style.display = "none" 
+}, 5000)
