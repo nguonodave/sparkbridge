1bd03607b4c29a71c8174ec827ae34a20f11114c
David Ochiel <nguonodave@gmail.com>
2024-09-28 06:19:57 +0300

    feat: add basic implementation of company_profile

--- users/views.py
+++ users/views.py
@@ -76,5 +76,10 @@ def customer_profile(request):
 
     return render(request, "users/profile.html", context)
 
-def company_profile(request):
-    pass
+def company_profile(request, username):
+    user = User.objects.get(username=username)
+    services = user.company.service_set.all().order_by('-date')
+    context = {
+        'services': services
+    }
+    return render(request, "users/profile.html", context)
