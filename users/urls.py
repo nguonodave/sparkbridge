a606774cddfdd4238d3c09eb76b6384e7aec82c0
David Ochiel <nguonodave@gmail.com>
2024-09-28 07:37:07 +0300

    refactor: change url path for customer profile

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/customer/', v.register_customer, name='register_customer'),
     path('register/company/', v.register_company, name='register_company'),
     path('login/', v.login_user, name='login_user'),
-    path('user/profile/', v.customer_profile, name='customer_profile'),
+    path('customer/profile/', v.customer_profile, name='customer_profile'),
     path('company/<str:username>/', v.company_profile, name='company_profile'),
 ]
