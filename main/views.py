395af2d03c829c024b8994ed31f037c19679df3a
David Ochiel <nguonodave@gmail.com>
2024-09-30 19:44:42 +0300

    feat: only display services with more than one request in most requested services page

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from services.models import Service
 from django.db.models import Count
 
 def home(request):
-    most_requested_services = Service.objects.annotate(request_count=Count('requestservice')).order_by('-request_count')
+    most_requested_services = Service.objects.annotate(request_count=Count('requestservice')).filter(request_count__gt=0).order_by('-request_count')
 
     context = {
         'most_requested_services': most_requested_services,
